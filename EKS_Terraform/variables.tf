3a98ffd947c9a6c40259c71e90099c1ac94eae8c
jameelP <106545059+jameelP@users.noreply.github.com>
2025-01-16 11:18:47 +0530

    variables.tf

--- EKS_Terraform/variables.tf
+++ EKS_Terraform/variables.tf
@@ -1,1 +1,12 @@
+variable "location" {
+  description = "The azure location to deploy to"
+  default     = "East US"
+  type        = string
+}
+
+variable "resource_group" {
+  description = "The name of the resource group"
+  default     = "devopsshack-resources"
+  type        = string
+}
 
