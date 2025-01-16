1cfdc2cf1094e4bbac3c624aa68412aaa5d3220f
jameelP <106545059+jameelP@users.noreply.github.com>
2025-01-16 11:18:24 +0530

    output.tf

--- EKS_Terraform/output.tf
+++ EKS_Terraform/output.tf
@@ -1,1 +1,12 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.example.name
+}
+
+output "kubernetes_cluster_id" {
+  value = azurerm_kubernetes_cluster.example.id
+}
+
+output "subnet_ids" {
+  value = azurerm_subnet.example[*].id
+}
 
