b936b84072fa84f4e2eca0ed42a70a8a7b10673a
jameelP <106545059+jameelP@users.noreply.github.com>
2025-01-16 11:25:19 +0530

    Update output.tf

--- EKS_Terraform/output.tf
+++ EKS_Terraform/output.tf
@@ -1,12 +1,11 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.example.name
+  value = azurerm_resource_group.devops_rg.name
 }
 
 output "kubernetes_cluster_id" {
-  value = azurerm_kubernetes_cluster.example.id
+  value = azurerm_kubernetes_cluster.devops_aks.id
 }
 
 output "subnet_ids" {
-  value = azurerm_subnet.example[*].id
+  value = azurerm_subnet.devops_subnet[*].id
 }
-
